385c3ebb918a03e09d1e59dd0e073399960aa2f0
Nandhini Ravisankar <nandhini.ravisankar@accenture.com>
2019-09-26 11:53:56 +0530

    ver 3.0

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Devops_Naren\\cucumber-skeleton-project-4.0\\cucumber-skeleton-project-4.0\\src\\test\\resources\\skeleton\\Login.feature",plugin = {"json:target/cucumber.jason"})
+@CucumberOptions(features="C:\\Devops_Naren\\cucumber-skeleton-project-4.0\\cucumber-skeleton-project-4.0\\src\\test\\resources\\skeleton\\Login.feature", plugin = {"json:target/cucumber.json"})
 public class RunCukesTest {
 }
